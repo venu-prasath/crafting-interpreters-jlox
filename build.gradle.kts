a407b8b933ebd3916380d3d675a86886a7527042
venu-prasath <venuprasth133@gmail.com>
2024-11-30 14:26:04 -0500

    scanner class defined for Lox

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("application")
+    id("java")
 }
 
 group = "com.craftinginterpreters.lox"
@@ -18,12 +18,11 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.register<JavaExec>("runLox") {
+tasks.register<JavaExec>("buildLox") {
     group = "application"
-    description = "Runs the Lox interpreter"
+    description = "build the Lox interpreter"
 
     mainClass.set("com.craftinginterpreters.lox.Lox")
-    //classpath = sourceSets["main"].runtimeClasspath
     classpath = sourceSets["main"].runtimeClasspath
 
     doFirst {
@@ -33,4 +32,28 @@ tasks.register<JavaExec>("runLox") {
     doLast {
         println("Lox interpreter finished")
     }
+}
+
+tasks.jar {
+    archiveBaseName.set("jlox")
+    archiveVersion.set("1.0-SNAPSHOT")
+
+    manifest {
+        attributes["Main-Class"] = "com.craftinginterpreters.lox.Lox"
+    }
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it)})
+}
+
+tasks.classes {
+    finalizedBy(tasks.jar)
+}
+
+tasks.register<JavaExec>("launchJar") {
+    dependsOn("buildLox")
+    group = "application"
+    description = "Runs the Lox interpreter"
+
+    mainClass.set("com.craftinginterpreters.lox.Lox")
+    classpath = files("${layout.buildDirectory}/libs/jlox-1.0-SNAPSHOT.jar")
+
 }
--- src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,10 @@
 package com.craftinginterpreters.lox;
 
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
@@ -12,6 +15,28 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
 
     Scanner(String source) {
         this.source = source;
@@ -67,11 +92,71 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
-
+            case '"':
+                string();
+                break;
             default:
+                if(isDigit(c)) {
+                    number();
+                } else if(isAlpha(c)) {
+                    identifier();
+                }
                 Lox.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private void identifier() {
+        while(isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(IDENTIFIER);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while(isDigit(peek())) advance();
+
+        if(peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while(isDigit(peek())) advance();
+        }
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if(current + 1 >= source.length()) return '\0';
+        return source.charAt(current+1);
+    }
+
+    private void string() {
+        while(peek() != '"' && !isAtEnd()) {
+            if(peek() == '\n') line++;
+            advance();
+        }
+        if(isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+        advance();
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     private char peek() {
