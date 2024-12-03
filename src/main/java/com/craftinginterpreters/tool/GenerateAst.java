d9d1a180a24e0acb647cfb117944462b5fa85a6e
venu-prasath <venuprasth133@gmail.com>
2024-12-03 15:47:02 -0500

    AstPrinter added

--- src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -32,14 +32,32 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for(String type: types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
+        //The base accept() method
+        writer.println();
+        writer.println("\tabstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(
+            PrintWriter writer, String baseName, List<String> types) {
+        writer.println("\tinterface Visitor<R> {");
+
+        for(String type: types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("\t\tR visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("\t}");
     }
 
     private static void defineType(
@@ -54,6 +72,13 @@ public class GenerateAst {
             String name = field.split(" ")[1];
             writer.println("\t\t\tthis." + name + " = " + name + ";");
         }
+        writer.println("\t\t}");
+
+        //visitor pattern
+        writer.println();
+        writer.println("\t\t@Override");
+        writer.println("\t\t<R> R accept(Visitor<R> visitor) {");
+        writer.println("\t\t\treturn visitor.visit" + className + baseName + "(this);");
         writer.println("\t\t}");
 
         //Fields
