a587c0be16a131bcbb70728374846bb3059f73ed
venu-prasath <venuprasth133@gmail.com>
2024-12-06 18:34:50 -0500

    Recursive descent parser added for basic expression parsing

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -51,10 +51,17 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if(hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
+
         // For now, just print the tokens.
-        for(Token token: tokens) {
-            System.out.println(token);
-        }
+//        for(Token token: tokens) {
+//            System.out.println(token);
+//        }
     }
 
     static void error(int line, String message) {
@@ -64,5 +71,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.out.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if(token.type == TokenType.EOF) {
+            report(token.line, "at end", message);
+        } else {
+            report(token.line, "at '" + token.lexeme + "'", message);
+        }
     }
 }
